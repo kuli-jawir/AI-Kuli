4fb0c4766a6de898db9b8bdf1c53954c84a88b32
unknown <hooklaja@gmail.com>
2022-12-01 13:48:51 +0700

    latest build

---
+++ pages/index.js
@@ -0,0 +1,71 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
+
+const Home = () => {
+const [userInput, setUserInput] = useState('');
+const [apiOutput, setApiOutput] = useState('')
+const [isGenerating, setIsGenerating] = useState(false)
+const callGenerateEndpoint = async () => {
+  setIsGenerating(true);
+
+  console.log("Calling OpenAI...")
+  const response = await fetch('/api/generate', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ userInput }),
+  });
+
+  const data = await response.json();
+  const { output } = data;
+  console.log("OpenAI replied...", output.text)
+
+  setApiOutput(`${output.text}`);
+  setIsGenerating(false);
+}
+const onUserChangedText = (event) => {
+  console.log(event.target.value);
+  setUserInput(event.target.value);
+ };
+  return (
+    <div className="root">
+      <div className="container">
+        <div className="header">
+          <div className="header-title">
+            <h1>Aldy Tools AI 😉</h1>
+          </div>
+          <div className="header-subtitle">
+            <h2>Help you to use Artificial Intelligence</h2>
+          </div>
+        </div>
+      </div>
+      <div className="prompt-container">
+          <textarea placeholder="start typing here" className="prompt-box" value={userInput} onChange={onUserChangedText} />;
+      </div>
+      <div className="prompt-buttons">
+      <a className="generate-button" onClick={callGenerateEndpoint}>
+        <div className="generate">
+          <p>Generate</p>
+        </div>
+      </a>
+  </div>
+      <div className="badge-container grow">
+        <a
+          href="https://buildspace.so/builds/ai-writer"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="badge">
+            <Image src={buildspaceLogo} alt="buildspace logo" />
+            <p>build by Aldy Argarena</p>
+          </div>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
